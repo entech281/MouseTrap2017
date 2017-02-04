93df94bbdc9a9503b25328959256e3d29e2f47bd
m andrews <mandrews@mandrews.org>
2017-02-04 18:39:56 -0500

    Get motors turning the right direction

--- src/RobotConstants.h
+++ src/RobotConstants.h
@@ -5,10 +5,10 @@
 const int c_joystickLeftPort = 0;
 
 // Drive motor ports
-const int c_flmotor_CANid = 1;
-const int c_rlmotor_CANid = 3;
-const int c_frmotor_CANid = 2;
-const int c_rrmotor_CANid = 4;
+const int c_flmotor_CANid = 2;
+const int c_rlmotor_CANid = 4;
+const int c_frmotor_CANid = 1;
+const int c_rrmotor_CANid = 3;
 
 // Drive motor inversion states
 const bool c_kflmotor_inversed = false;
